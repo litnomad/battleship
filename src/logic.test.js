63f862be912b286562580138d539c62992d6b4bf
litnomad <128262375+litnomad@users.noreply.github.com>
2026-08-06 12:36:19 -0400

    test: computerAttacks() generates random coordinates between 0 to 9

--- src/logic.test.js
+++ src/logic.test.js
@@ -1,4 +1,11 @@
-import { createBoard, placeShips, Ship, Player } from "./logic.js";
+import { experiments } from "webpack";
+import {
+  createBoard,
+  placeShips,
+  Ship,
+  Player,
+  computerAttacks,
+} from "./logic.js";
 
 test("createBoard returns a 10x10 board", () => {
   const board = createBoard();
@@ -11,8 +18,8 @@ test("placeShips adds ships into the board using coordinates", () => {
   const board = createBoard();
   const placements = placeShips(
     [
-      [[0, 0], 3],
-      [[5, 2], 2],
+      [[0, 0], 3, "horizontal"],
+      [[5, 2], 2, "vertical"],
     ],
     board,
   );
@@ -23,18 +30,18 @@ test("placeShips adds ships into the board using coordinates", () => {
 
 describe("new player gets a gameboard", () => {
   const player = new Player([
-    [[0, 8], 3],
-    [[5, 0], 4],
-    [[9, 5], 2],
-    [[5, 2], 5],
-    [[9, 7], 3],
+    [[0, 8], 3, "horizontal"],
+    [[5, 0], 4, "vertical"],
+    [[9, 0], 2, "vertical"],
+    [[7, 0], 1, "horizontal"],
+    [[9, 7], 3, "vertical"],
   ]);
 
   test("ships placed for new player", () => {
     expect(player.board.getBoard()[0][8][0]).toBeInstanceOf(Ship);
     expect(player.board.getBoard()[5][0][0]).toBeInstanceOf(Ship);
-    expect(player.board.getBoard()[9][5][0]).toBeInstanceOf(Ship);
-    expect(player.board.getBoard()[5][2][0]).toBeInstanceOf(Ship);
+    expect(player.board.getBoard()[9][0][0]).toBeInstanceOf(Ship);
+    expect(player.board.getBoard()[7][0][0]).toBeInstanceOf(Ship);
     expect(player.board.getBoard()[9][7][0]).toBeInstanceOf(Ship);
   });
 
@@ -54,7 +61,28 @@ describe("new player gets a gameboard", () => {
     player.board.receiveAttack(0, 8);
     player.board.receiveAttack(0, 8);
     player.board.receiveAttack(0, 8);
-    
-    expect(player.board.getBoard()[0][8][0].sunk).toBe(true)
-  })
+
+    expect(player.board.getBoard()[0][8][0].sunk).toBe(true);
+  });
+
+  test("computerAttacks generates random x,y coordinates", () => {
+    const x = computerAttacks().column;
+    const y = computerAttacks().row;
+
+    expect(Number.isInteger(x)).toBe(true);
+    expect(Number.isInteger(y)).toBe(true);
+  });
+
+  test("computerAttacks generates random x,y coordinates between 0-9", () => {
+    const integars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const x = computerAttacks().column;
+    const y = computerAttacks().row;
+
+    const a = integars.find((e) => e === x);
+    const b = integars.find((e) => e === y);
+
+    expect(x).toBe(a);
+    expect(y).toBe(b);
+  });
 });
